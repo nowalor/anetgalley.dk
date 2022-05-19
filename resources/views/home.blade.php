4842cae547f69e87b119bac0768230b239c31220
nikulaswalor <no@walor.io>
2022-05-19 16:41:08 +0200

    progress with cta

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,5 +2,19 @@
 @section('title', 'Homepage')
 
 @section('content')
-    content goes here
+    <div class="container">
+        <div class="cta">
+            <div class="cta__left">
+                <h1 class="cta__left-heading">Art by Anette Andersen</h1>
+                <h3 class="cta__left-tagline">Art is a media for reflection, existensial expansion... a pulse - pleasure - provocation....a sense of life</h3>
+                <div class="cta__left-buttons">
+                    <a href="#" class="button button__black">SHOP &rarr;</a>
+                    <a href="#" class="button button__white">GALLERY &rarr;</a>
+                </div>
+            </div>
+            <div class="cta__right">
+                <img src="{{ asset('img/placeholder.jpg') }}" alt="" class="cta__right-img">
+            </div>
+        </div>
+    </div>
 @endsection
