0d19c6c1402c349bb1757975c47f621155f0f359
nikulaswalor <no@walor.io>
2022-05-19 21:27:01 +0200

    progress

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -6,15 +6,15 @@
         <div class="container">
             <div class="products">
                 @foreach($products as $product)
-                    <div class="product">
+                    <div class="product pt-8">
                         <h2 class="heading-lg medium">{{ $product->name }}</h2>
-                        <div class="product__price-box">
-                            <p class="product__price">{{$product->price }}DKK</p>
+                        <div class="product__price-box mt-2">
+                            <p class="product__price medium">{{$product->price }}DKK</p>
                         </div>
-                        <p class="product__description">
+                        <p class="product__description mt-2">
                             {{ str()->words($product->description, 50) }}
                         </p>
-                        <img src="{{ asset('img/placeholder.jpg') }}" alt="" class="product__image">
+                        <img src="{{ asset('img/placeholder.jpg') }}" alt="" class="product__image mt-2">
                         <a href="{{route('products.show', $product->id)}}" class="button button__pink">BUY</a>
                     </div>
                 @endforeach
