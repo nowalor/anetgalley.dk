30d37982f3ebf7304c291c4c10384064816c93f7
nikulaswalor <no@walor.io>
2022-05-25 11:07:44 +0200

    added login as guest routes

--- routes/web.php
+++ routes/web.php
@@ -13,8 +13,14 @@ Route::resource('products', ProductController::class,
     ['only' => ['index', 'show']]
 );
 
-Route::get('login', [LoginController::class, 'showLoginForm'])->name('auth.show-login');
-Route::post('login', [LoginController::class, 'login'])->name('auth.login');
+
+
+Route::middleware('guest')->group(function() {
+    Route::get('login', [LoginController::class, 'showLoginForm'])->name('auth.show-login');
+    Route::post('login', [LoginController::class, 'login'])->name('auth.login');
+});
+
+
 Route::post('logout', [LoginController::class, 'logout'])->name('auth.logout');
 
 // Admin routes
