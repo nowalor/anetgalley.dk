000187a1515a14c0666f6957f5c1a5012846c98d
nikulaswalor <no@walor.io>
2022-05-25 11:16:49 +0200

    some work on login form

--- resources/views/admin/login.blade.php
+++ resources/views/admin/login.blade.php
@@ -1,29 +1,32 @@
 @extends('app')
 @section('extra-links')
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet"
+          integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
 @endsection
 @section('title', 'Login')
 @section('content')
     <div class="container" style="width: 37rem;">
-        <h1 class="pt-8 ttu ">Login</h1>
+        <h1 class="heading-lg pt-8 ttu ">Login</h1>
         <form method="POST" action="{{ route('auth.login') }}">
             @csrf
-            <div class="form-group">
+            <div class="form-group pt-2">
                 <label class="label" for="exampleInputEmail1">Email address</label>
-                <input value="{{ old('email') }}" name="email" type="email" class="input" id="exampleInputEmail1">
+                <input value="{{ old('email') }}" name="email" type="email"
+                       class="input @error('email') validation-error-input @enderror" id="exampleInputEmail1">
                 @error('email')
-                    <p class="validation-error">
-                        {{ $message }}
-                    </p>
+                <p class="validation-error">
+                    {{ $message }}
+                </p>
                 @enderror
             </div>
             <div class="form-group">
-                <label  class="label" for="exampleInputPassword1">Password</label>
-                <input name="password" type="password" class="input" id="exampleInputPassword1">
+                <label class="label" for="exampleInputPassword1">Password</label>
+                <input name="password" type="password" class="input @error('password') validation-error-input @enderror"
+                       id="exampleInputPassword1">
                 @error('password')
-                    <p class="validation-error">
-                        {{ $message }}
-                    </p>
+                <p class="validation-error">
+                    {{ $message }}
+                </p>
                 @enderror
             </div>
 
--- routes/web.php
+++ routes/web.php
@@ -13,13 +13,11 @@ Route::resource('products', ProductController::class,
     ['only' => ['index', 'show']]
 );
 
-
-
+// Guest routes
 Route::middleware('guest')->group(function() {
     Route::get('login', [LoginController::class, 'showLoginForm'])->name('auth.show-login');
     Route::post('login', [LoginController::class, 'login'])->name('auth.login');
 });
-
 
 Route::post('logout', [LoginController::class, 'logout'])->name('auth.logout');
 
