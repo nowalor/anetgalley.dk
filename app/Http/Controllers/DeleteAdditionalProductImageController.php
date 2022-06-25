d19bc64aa2d3342e24d2ed203824fdab201e45fa
nowalor <no@walor.io>
2022-06-25 10:02:30 +0000

    deleting additonal image from storage

--- app/Http/Controllers/DeleteAdditionalProductImageController.php
+++ app/Http/Controllers/DeleteAdditionalProductImageController.php
@@ -4,11 +4,16 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\ProductAdditionalImage;
+use Illuminate\Support\Facades\Storage;
 
 class DeleteAdditionalProductImageController extends Controller
 {
     public function __invoke(ProductAdditionalImage $productAdditionalImage)
     {
+        Storage::disk('public')->deleteDirectory(
+            'product-images/' . $productAdditionalImage->product_id . '/additional/' . $productAdditionalImage->id . '/',
+        );
+
         $productAdditionalImage->delete();
 
         return redirect()->back()->with('additional_image_deleted', 'Image deleted');
