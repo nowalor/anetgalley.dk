beabc6332961c11d1b9481c80f50575db40434e2
nowalor <no@walor.io>
2022-06-27 22:56:15 +0200

    single product responsive

---
+++ app/Http/Controllers/EditLandingPageImageController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
+use App\Models\HomepageInformation;
+
+class EditLandingPageImageController extends Controller
+{
+    public function __invoke()
+    {
+    }
+}
