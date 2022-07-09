f685925c79fbf66ef4e75c2fc32c58a1a4162421
nowalor <no@walor.io>
2022-07-09 16:43:13 +0200

    events page coming from translation file

--- resources/views/events/index.blade.php
+++ resources/views/events/index.blade.php
@@ -4,21 +4,21 @@
     <div class="container">
         <div class="events-page">
             <div class="breadcrumbs">
-                <a href="{{ route('home') }}" class="breadcrumbs__link">Home</a>
+                <a href="{{ route('home') }}" class="breadcrumbs__link">{{ __('events.home') }}</a>
                 <p class="breadcrumbs__slash">/</p>
-                <a href="{{ route('events.index') }}" class="breadcrumbs__link breadcrumbs__link-active">Events</a>
+                <a href="{{ route('events.index') }}" class="breadcrumbs__link breadcrumbs__link-active">{{ __('events.events') }}</a>
             </div>
 
             <ul class="filter-links pt-2">
             <li class="filter-links__item">
                 <a href="{{ route('events.index') }}"
-                   class="filter-links__link font16 medium ttu @if(Request::query('filter') == 0) active @endif }}">Upcoming</a>
+                   class="filter-links__link font16 medium ttu @if(Request::query('filter') == 0) active @endif }}">{{ __('events.upcoming') }}</a>
 
             </li>
 
             <li class="filter-links__item">
                 <a href="{{ route('events.index', ['filter' => 'all']) }}"
-                   class="filter-links__link font16 medium ttu @if(Request::query('filter') === 'all') active @endif">All</a>
+                   class="filter-links__link font16 medium ttu @if(Request::query('filter') === 'all') active @endif">{{ __('events.all') }}</a>
             </li>
         </ul>
 
@@ -29,7 +29,7 @@
                         <h3 class="heading-md medium pt-2">{{ $event->name }}</h3>
                         <p class="pt-1 font16"> {{ $event->short_description }}</p>
                         <p class="pt-2 font14 light pb-4">{{ $event->formattedFromToDate }}</p>
-                        <a target="_blank" href=" {{ $event->event_url }} " class="button button__black width100">View event</a>
+                        <a target="_blank" href=" {{ $event->event_url }} " class="button button__black width100">{{ __('events.view_events') }}</a>
                     </div>
                 @empty
                     empty...
