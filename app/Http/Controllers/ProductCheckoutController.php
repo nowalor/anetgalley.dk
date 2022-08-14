e9b56a605cc676764171ff848293600e4347b724
nowalor <no@walor.io>
2022-08-14 13:55:49 +0200

    test

--- app/Http/Controllers/ProductCheckoutController.php
+++ app/Http/Controllers/ProductCheckoutController.php
@@ -4,9 +4,6 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\CreateProductCheckoutRequest;
 use App\Models\Product;
-use LaravelDaily\Invoices\Invoice;
-use LaravelDaily\Invoices\Classes\Buyer;
-use LaravelDaily\Invoices\Classes\InvoiceItem;
 
 class ProductCheckoutController extends Controller
 {
@@ -15,24 +12,6 @@ class ProductCheckoutController extends Controller
     {
         $validated = $request->validated();
 
-        $buyer = new Buyer([
-            'name' => $validated['name'],
-            'custom_fields' => [
-                'email' => $validated['email'],
-            ],
-        ]);
-
-        $item = (new InvoiceItem())->title('Service 1')->pricePerUnit(2);
-
-        $invoice = Invoice::make()
-            ->buyer($buyer)
-            ->discountByPercent(10)
-            ->taxRate(15)
-            ->shipping(1.99)
-            ->addItem($item);
-
-
-        return $invoice->stream();
 
         return $validated;
     }
