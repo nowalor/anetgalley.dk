f90977edf9a6b78a81490f5a78a9bfab973d366a
nowalor <no@walor.io>
2023-06-14 21:34:01 +0200

    feat: padding on admin.product.update

--- resources/views/admin/products/edit.blade.php
+++ resources/views/admin/products/edit.blade.php
@@ -6,7 +6,7 @@
 @endsection
 @section('title', 'Admin')
 @section('content')
-    <div class="container center">
+    <div class="container center pb-8">
         <h1 class="heading-lg pt-2">Create a new product</h1>
         <form class="pt-4" action="{{ route('admin.products.update', $product) }}" method="POST"
               enctype="multipart/form-data"
@@ -120,7 +120,7 @@
                 @enderror
             </div>
 
-            <button type="submit" class="mt-2 button-pink-100 ttu">Submit</button>
+            <button type="submit" class="mt-2 button-pink-100 ttu mb-8">Submit</button>
         </form>
     </div>
 @endsection
